9bddc30f03345ac3514272a0dd034843d7bf9d93
celebra09 <77697810+celebra09@users.noreply.github.com>
2021-04-15 17:13:03 +0100

    Showing current temp

---
+++ src/app.js
@@ -0,0 +1,23 @@
+function displayTemperature(response){
+    console.log (response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+     let humidityElement = document.querySelector("#humidity");
+      let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+   cityElement.innerHTML=(response.data.name);
+   descriptionElement.innerHTML=(response.data.weather[0].description);
+   humidityElement.innerHTML=(response.data.main.humidity);
+   windElement.innerHTML=Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "ce50cde028d0f50f364b5558bd7f0fe1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
+
+
+
+
